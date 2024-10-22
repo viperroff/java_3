c65c813518c50caa2489f52bafba40f8abf1dd2b
K0lmykov <alosturd@gmail.com>
2024-10-22 13:37:22 +0400

    switched to 1.0 version

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Human human1 = new Human("Pit", 898);
         System.out.println(human1);
-        System.out.println("this is 1.1 version!");
+        System.out.println("this is 1.0 version!");
     }
 }
